d2474a2e6ad04dc1d21db5330233770edfef35d7
Heo Nayeong <gjsk132@naver.com>
2024-07-08 22:14:48 +0900

    안드로이드 버전 및 자바 버전 수정

--- app/src/main/java/com/example/hicure/MainActivity.kt
+++ app/src/main/java/com/example/hicure/MainActivity.kt
@@ -2,27 +2,21 @@ package com.example.hicure
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hicure.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate((layoutInflater)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
 
-        val button1:Button=findViewById(R.id.button1)
-        button1.setOnClickListener{
-            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
-
-        }
     }
 }
