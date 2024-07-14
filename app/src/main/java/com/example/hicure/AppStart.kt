7a2caa56f09d1ce871c01d4076c850a8d1552017
Heo Nayeong <gjsk132@naver.com>
2024-07-14 23:50:04 +0900

    Developing splash screen feature 2

--- app/src/main/java/com/example/hicure/AppStart.kt
+++ app/src/main/java/com/example/hicure/AppStart.kt
@@ -1,6 +1,9 @@
 package com.example.hicure
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +17,24 @@ class AppStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // Splash Screen
+        /*Handler().postDelayed(Runnable {
+            val i = Intent(this@AppStart,MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 5000)*/
+    }
+
+    // Touch Screen
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                val i = Intent(this@AppStart,MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
