83757155a18f2e194ecb0647b497cac90b9602eb
Heo Nayeong <gjsk132@naver.com>
2024-07-23 14:04:11 +0900

    Created UserInfo Activity

---
+++ app/src/main/java/com/example/hicure/UserInfo.kt
@@ -0,0 +1,20 @@
+package com.example.hicure
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hicure.databinding.ActivityUserInfoBinding
+
+class UserInfo : AppCompatActivity() {
+
+    val binding: ActivityUserInfoBinding by lazy { ActivityUserInfoBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+    }
+}
