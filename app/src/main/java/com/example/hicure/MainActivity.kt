4e7d69127f777dfc49fb36068e34da29aa972c09
hyeon <hyeonju0925@naver.com>
2024-07-23 11:30:19 +0900

    save

--- app/src/main/java/com/example/hicure/MainActivity.kt
+++ app/src/main/java/com/example/hicure/MainActivity.kt
@@ -1,28 +1,67 @@
 package com.example.hicure
 
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.hicure.databinding.ActivityMainBinding
 import android.view.ViewTreeObserver
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import android.widget.FrameLayout
-import com.example.hicure.databinding.ActivityAppStartBinding
-import java.sql.Time
-import java.sql.Timestamp
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+
 
 class MainActivity : AppCompatActivity() {
 
     val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate((layoutInflater)) }
+    private lateinit var pieChart: PieChart
 
+    private fun setBarChart() {
+        binding.pieChart.setUsePercentValues(true)
+
+        // data set
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(14f, "Apple"))
+        entries.add(PieEntry(22f, "Orange"))
+        entries.add(PieEntry(7f, "Mango"))
+        entries.add(PieEntry(31f, "RedOrange"))
+        entries.add(PieEntry(26f, "Other"))
+
+
+        // add a lot of colors
+        val colorsItems = ArrayList<Int>()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colorsItems.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colorsItems.add(c)
+        for (c in ColorTemplate.COLORFUL_COLORS) colorsItems.add(c)
+        for (c in ColorTemplate.LIBERTY_COLORS) colorsItems.add(c)
+        for (c in ColorTemplate.PASTEL_COLORS) colorsItems.add(c)
+        colorsItems.add(ColorTemplate.getHoloBlue())
+
+        val pieDataSet = PieDataSet(entries, "")
+        pieDataSet.apply {
+            colors = colorsItems
+            valueTextColor = Color.BLACK
+            valueTextSize = 18f
+
+        }
+
+        val pieData = PieData(pieDataSet)
+        binding.pieChart.apply {
+            data = pieData
+            description.isEnabled = false
+            isRotationEnabled = false
+            setEntryLabelColor(Color.BLACK)
+            setCenterTextSize(20f)
+            animateY(1400, Easing.EaseInOutQuad)
+            animate()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +76,8 @@ class MainActivity : AppCompatActivity() {
         textViewTime.text = currentTime
 
         "원하는 타이틀 입력".also { binding.actionTitle.text = it }
+
+
 
         binding.actionTitle.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
 
