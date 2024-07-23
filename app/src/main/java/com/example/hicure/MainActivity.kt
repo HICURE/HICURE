f2ce417e38459fc9b869d2d9e2244bae82135b0e
hyeon <hyeonju0925@naver.com>
2024-07-23 17:16:57 +0900

    unimage

--- app/src/main/java/com/example/hicure/MainActivity.kt
+++ app/src/main/java/com/example/hicure/MainActivity.kt
@@ -9,66 +9,37 @@ import android.view.ViewTreeObserver
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import com.github.mikephil.charting.animation.Easing
+
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
 
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import java.util.ArrayList
+
+data class ChartData(
+    var lableData: String = "",
+    var lineData: Double = 0.0
+)
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = this.javaClass.simpleName
+    lateinit var lineChart: LineChart
+    private val chartData = ArrayList<ChartData>()
 
-    val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate((layoutInflater)) }
-    private lateinit var pieChart: PieChart
-
-    private fun setBarChart() {
-        binding.pieChart.setUsePercentValues(true)
-
-        // data set
-        val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(14f, "Apple"))
-        entries.add(PieEntry(22f, "Orange"))
-        entries.add(PieEntry(7f, "Mango"))
-        entries.add(PieEntry(31f, "RedOrange"))
-        entries.add(PieEntry(26f, "Other"))
-
-
-        // add a lot of colors
-        val colorsItems = ArrayList<Int>()
-        for (c in ColorTemplate.VORDIPLOM_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.JOYFUL_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.COLORFUL_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.LIBERTY_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.PASTEL_COLORS) colorsItems.add(c)
-        colorsItems.add(ColorTemplate.getHoloBlue())
-
-        val pieDataSet = PieDataSet(entries, "")
-        pieDataSet.apply {
-            colors = colorsItems
-            valueTextColor = Color.BLACK
-            valueTextSize = 18f
-
-        }
-
-        val pieData = PieData(pieDataSet)
-        binding.pieChart.apply {
-            data = pieData
-            description.isEnabled = false
-            isRotationEnabled = false
-            setEntryLabelColor(Color.BLACK)
-            setCenterTextSize(20f)
-            animateY(1400, Easing.EaseInOutQuad)
-            animate()
-        }
-    }
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val currentDate = getCurrentDate()
-        val currentTime= getCurrentTime()
+        val currentTime = getCurrentTime()
 
         val textViewDate: TextView = findViewById(R.id.textViewDate)
         textViewDate.text = currentDate
@@ -77,33 +48,91 @@ class MainActivity : AppCompatActivity() {
 
         "원하는 타이틀 입력".also { binding.actionTitle.text = it }
 
-
-
         binding.actionTitle.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                binding.actionTitle.viewTreeObserver.removeOnGlobalLayoutListener(this)
 
-               override fun onGlobalLayout(){
-                    binding.actionTitle.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                val actionTextWidth = binding.actionTitle.width
+                binding.actionTitle.width = actionTextWidth + 10
 
-                   val actionTextWidth = binding.actionTitle.width
-
-                   binding.actionTitle.width = actionTextWidth + 10
-
-                   // binding.mainText.text = "$actionTextWidth"
-
-                   val layoutParams = binding.behindTitle.layoutParams
-                   layoutParams.width = actionTextWidth + 30
-                   binding.behindTitle.layoutParams = layoutParams
-
-               }
+                val layoutParams = binding.behindTitle.layoutParams
+                layoutParams.width = actionTextWidth + 30
+                binding.behindTitle.layoutParams = layoutParams
+            }
         })
 
+        setupPieChart()
+        setupLineChart()
     }
+
     private fun getCurrentDate(): String {
         val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault())
         return dateFormat.format(Date())
     }
+
     private fun getCurrentTime(): String {
         val dateFormat = SimpleDateFormat("hh : mm", Locale.getDefault())
         return dateFormat.format(Date())
+    }
+
+    private fun setupPieChart() {
+        val pieChart = findViewById<PieChart>(R.id.pieChart)
+
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(40f, ""))
+        entries.add(PieEntry(60f, ""))
+
+        val dataSet = PieDataSet(entries, "Election Results")
+        dataSet.colors = listOf(Color.GRAY, Color.LTGRAY)
+        dataSet.setDrawValues(false)
+
+        // 도넛 차트 효과를 위해 가운데 원을 비웁니다.
+        pieChart.isDrawHoleEnabled = true
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+
+        pieChart.description.isEnabled = false
+        pieChart.legend.isEnabled = false
+
+        val data = PieData(dataSet)
+        pieChart.data = data
+        pieChart.invalidate()
+    }
+
+    private fun setupLineChart() {
+        lineChart = findViewById(R.id.linechart)
+
+        // LineChart 데이터 초기화
+        chartData.clear()
+        addChartItem("1월", 7.9)
+        addChartItem("2월", 8.2)
+        addChartItem("3월", 8.3)
+        addChartItem("4월", 8.5)
+        addChartItem("5월", 7.3)
+
+        val entries = mutableListOf<Entry>()
+
+        for (item in chartData) {
+            entries.add(Entry(item.lableData.replace("[^\\d.]".toRegex(), "").toFloat(), item.lineData.toFloat()))
+        }
+
+        val lineDataSet = LineDataSet(entries, "")
+        lineDataSet.color = Color.BLUE // LineChart에서 Line Color 설정
+        lineDataSet.setCircleColor(Color.DKGRAY) // LineChart에서 Line Circle Color 설정
+        lineDataSet.setCircleHoleColor(Color.DKGRAY) // LineChart에서 Line Hole Circle Color 설정
+
+        val dataSets = ArrayList<ILineDataSet>()
+        dataSets.add(lineDataSet)
+
+        val data = LineData(dataSets)
+
+        lineChart.data = data
+        lineChart.description.isEnabled = false
+        lineChart.invalidate() // 차트를 새로고침합니다.
+    }
+
+    private fun addChartItem(labelItem: String, dataItem: Double) {
+        val item = ChartData(labelItem, dataItem)
+        chartData.add(item)
     }
 }
