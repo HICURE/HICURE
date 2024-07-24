907afa05c3f8fc1273300724f6cf4585cbac347d
김수현 <suhyun_113@naver.com>
2024-07-24 19:59:30 +0900

    Chore: revise kt file

--- app/src/main/java/com/example/hicure/AlarmList.kt
+++ app/src/main/java/com/example/hicure/AlarmList.kt
@@ -32,8 +32,8 @@ class AlarmList : AppCompatActivity() {
         alarmSwitchPink = findViewById(R.id.alarmSwitchPink)
 
 
-        // 초기 스타일 설정
-        setAlarmBoxStyle("grey")
+//        // 초기 스타일 설정
+//        setAlarmBoxStyle("grey")
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -42,23 +42,23 @@ class AlarmList : AppCompatActivity() {
         }
     }
 
-    private fun setAlarmBoxStyle(style: String) {
-        when (style) {
-            "blue" -> {
-                alarmBoxLayoutBlue.setBackgroundResource(R.drawable.alarm_box_blue)
-                alarmBoxLayoutBlue.setTag(R.attr.alarmBoxStyle, 0)
-                alarmSwitchBlue.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_blue)
-            }
-            "yellow" -> {
-                alarmBoxLayoutYellow.setBackgroundResource(R.drawable.alarm_box_yellow)
-                alarmBoxLayoutYellow.setTag(R.attr.alarmBoxStyle, 1)
-                alarmSwitchYellow.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_yellow)
-            }
-            "pink" -> {
-                alarmBoxLayoutPink.setBackgroundResource(R.drawable.alarm_box_pink)
-                alarmBoxLayoutPink.setTag(R.attr.alarmBoxStyle, 2)
-                alarmSwitchPink.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_pink)
-            }
-        }
-    }
+//    private fun setAlarmBoxStyle(style: String) {
+//        when (style) {
+//            "blue" -> {
+//                alarmBoxLayoutBlue.setBackgroundResource(R.drawable.alarm_box_blue)
+//                alarmBoxLayoutBlue.setTag(R.attr.alarmBoxStyle, 0)
+//                alarmSwitchBlue.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_blue)
+//            }
+//            "yellow" -> {
+//                alarmBoxLayoutYellow.setBackgroundResource(R.drawable.alarm_box_yellow)
+//                alarmBoxLayoutYellow.setTag(R.attr.alarmBoxStyle, 1)
+//                alarmSwitchYellow.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_yellow)
+//            }
+//            "pink" -> {
+//                alarmBoxLayoutPink.setBackgroundResource(R.drawable.alarm_box_pink)
+//                alarmBoxLayoutPink.setTag(R.attr.alarmBoxStyle, 2)
+//                alarmSwitchPink.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_pink)
+//            }
+//        }
+//    }
 }
