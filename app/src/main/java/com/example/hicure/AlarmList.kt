15f8be5a7f4d2087cb9ab3d87c002c182c908b11
hyeon <hyeonju0925@naver.com>
2024-07-29 12:09:32 +0900

    add calendar Page

--- app/src/main/java/com/example/hicure/AlarmList.kt
+++ app/src/main/java/com/example/hicure/AlarmList.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.hicure.databinding.ActivityAlarmListBinding
 
 
