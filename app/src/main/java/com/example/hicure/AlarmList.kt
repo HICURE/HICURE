aa64e9f2c809c9bd28e3cc70c01a59cf5eb406e3
김수현 <suhyun_113@naver.com>
2024-07-29 11:53:22 +0900

    Feat: add an event to the AlarmList file that moves to setAlarm file

--- app/src/main/java/com/example/hicure/AlarmList.kt
+++ app/src/main/java/com/example/hicure/AlarmList.kt
@@ -1,36 +1,35 @@
 package com.example.hicure
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.ViewTreeObserver
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.hicure.databinding.ActivityAlarmListBinding
-
 
 class AlarmList : AppCompatActivity() {
-//    private lateinit var alarmBoxLayoutBlue: ConstraintLayout
-//    private lateinit var alarmBoxLayoutYellow: ConstraintLayout
-//    private lateinit var alarmBoxLayoutPink: ConstraintLayout
-//    private lateinit var alarmSwitchBlue: Switch
-//    private lateinit var alarmSwitchYellow: Switch
-//    private lateinit var alarmSwitchPink: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_alarm_list)
 
-//        alarmBoxLayoutBlue = findViewById(R.id.alarmBoxLayoutBlue)
-//        alarmBoxLayoutYellow = findViewById(R.id.alarmBoxLayoutYellow)
-//        alarmBoxLayoutPink = findViewById(R.id.alarmBoxLayoutPink)
-//        alarmSwitchBlue = findViewById(R.id.alarmSwitchBlue)
-//        alarmSwitchYellow = findViewById(R.id.alarmSwitchYellow)
-//        alarmSwitchPink = findViewById(R.id.alarmSwitchPink)
+        val alarmBoxBlue = findViewById<CardView>(R.id.alarmBoxBlue)
+        val alarmBoxYellow = findViewById<CardView>(R.id.alarmBoxYellow)
+        val alarmBoxPink = findViewById<CardView>(R.id.alarmBoxPink)
 
-//        // 초기 스타일 설정
-//        setAlarmBoxStyle("grey")
+        alarmBoxBlue.setOnClickListener {
+            navigateToSetAlarm()
+        }
+
+        alarmBoxYellow.setOnClickListener {
+            navigateToSetAlarm()
+        }
+
+        alarmBoxPink.setOnClickListener {
+            navigateToSetAlarm()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -38,25 +37,8 @@ class AlarmList : AppCompatActivity() {
             insets
         }
     }
-
-
-//    private fun setAlarmBoxStyle(style: String) {
-//        when (style) {
-//            "blue" -> {
-//                alarmBoxLayoutBlue.setBackgroundResource(R.drawable.alarm_box_blue)
-//                alarmBoxLayoutBlue.setTag(R.attr.alarmBoxStyle, 0)
-//                alarmSwitchBlue.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_blue)
-//            }
-//            "yellow" -> {
-//                alarmBoxLayoutYellow.setBackgroundResource(R.drawable.alarm_box_yellow)
-//                alarmBoxLayoutYellow.setTag(R.attr.alarmBoxStyle, 1)
-//                alarmSwitchYellow.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_yellow)
-//            }
-//            "pink" -> {
-//                alarmBoxLayoutPink.setBackgroundResource(R.drawable.alarm_box_pink)
-//                alarmBoxLayoutPink.setTag(R.attr.alarmBoxStyle, 2)
-//                alarmSwitchPink.trackDrawable =ContextCompat.getDrawable(this, R.drawable.alarm_switch_track_on_pink)
-//            }
-//        }
-//    }
+    private fun navigateToSetAlarm() {
+        val intent = Intent(this, SetAlarm::class.java)
+        startActivity(intent)
+    }
 }
