df333fa3d9d98785b77fc82455891f3c0c2855f0
김수현 <suhyun_113@naver.com>
2024-08-05 15:56:35 +0900

    Feat: add saveButton event

--- app/src/main/java/com/example/hicure/SetAlarm.kt
+++ app/src/main/java/com/example/hicure/SetAlarm.kt
@@ -1,5 +1,6 @@
 package com.example.hicure
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
@@ -15,14 +16,10 @@ class SetAlarm : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_alarm)
 
-        val timePicker = findViewById<CustomTimePickerdd>(R.id.customTimePicker)
-        val saveButton = findViewById<Button>(R.id.saveButton)
-        val soundVibrationSwitch = findViewById<Switch>(R.id.soundVibrationSwitch)
-
-        saveButton.setOnClickListener {
-            val selectedTime = timePicker.getSelectedTime()
-            Toast.makeText(this, "Selected Time: $selectedTime", Toast.LENGTH_SHORT).show()
-        }
+        val timePicker = findViewById<CustomTimePickerdd>(R.id.customTimePicker) // 시간 설정
+        val cancelButton = findViewById<Button>(R.id.cancelButton) // 취소 버튼
+        val saveButton = findViewById<Button>(R.id.saveButton) // 저장 버튼
+        val soundVibrationSwitch = findViewById<Switch>(R.id.soundVibrationSwitch) // 소리 및 진동 토글 버튼
 
         val boxDrawableResId = intent.getIntExtra("EXTRA_BOX_COLOR", R.drawable.set_alarm_box_blue)
         val switchDrawableResId = intent.getIntExtra("EXTRA_SWITCH_COLOR", R.drawable.alarm_switch_track_on_blue)
@@ -39,8 +36,21 @@ class SetAlarm : AppCompatActivity() {
         boxLayout.background = boxDrawable
 
         soundVibrationSwitch.trackDrawable = switchDrawable
-//        soundVibrationSwitch.thumbDrawable = switchDrawable
-
         saveButton.background = buttonDrawable
+
+        // 취소 버튼
+        cancelButton.setOnClickListener {
+            val selectedTime = timePicker.getSelectedTime()
+            Toast.makeText(this, "Selected Time: $selectedTime", Toast.LENGTH_SHORT).show()
+        }
+
+        // 저장 버튼
+        saveButton.setOnClickListener {
+            val selectedTime = timePicker.getSelectedTime()
+            val resultIntent = Intent()
+            resultIntent.putExtra("EXTRA_SELECTED_TIME", selectedTime)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
