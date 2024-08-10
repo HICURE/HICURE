ea5c55a6713c8366e168af3d677366b4339e7b5d
김수현 <suhyun_113@naver.com>
2024-08-10 22:06:46 +0900

    Feat: change alarmList alarmOnOff toggle

--- app/src/main/java/com/example/hicure/AlarmList.kt
+++ app/src/main/java/com/example/hicure/AlarmList.kt
@@ -4,80 +4,59 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import android.Manifest
-import android.content.pm.PackageManager
-import android.widget.Switch
-import androidx.core.content.ContextCompat
-import androidx.core.app.ActivityCompat
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AlarmList : AppCompatActivity() {
 
     private var lastClickedAlarmBox: Int = 0
-
-    private val PERMISSION_REQUEST_CODE = 1001
+    private var initialTime: String = ""
+    private var AMPMText: String = ""
+    private var initialLabelText: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_list)
         setupAlarmBoxListeners()
-
-//        checkAndRequestPermissions()
     }
-
-//    private fun checkAndRequestPermissions() {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SET_ALARM) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SET_ALARM), PERMISSION_REQUEST_CODE)
-//        } else {
-//            // 권한이 이미 허용됨
-//            setupAlarmBoxListeners()
-//        }
-//    }
-
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == PERMISSION_REQUEST_CODE) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                // 권한이 허용됨
-//                setupAlarmBoxListeners()
-//            } else {
-//                // 권한이 거부됨, 사용자에게 권한이 필요함을 안내
-//            }
-//        }
-//    }
 
     private fun setupAlarmBoxListeners() {
         findViewById<CardView>(R.id.alarmBoxBlue).setOnClickListener {
             lastClickedAlarmBox = R.id.alarmBoxBlue
+            initialTime = getCurrentTimeForBox(R.id.alarmBoxBlue)
+            AMPMText = getAMPMForBox(R.id.alarmBoxBlue)
+            initialLabelText = getLabelTextForBox(R.id.alarmBoxBlue)
             navigateToSetAlarm(R.drawable.set_alarm_box_blue, R.drawable.alarm_switch_track_on_blue, R.drawable.set_alarm_save_button_box_blue)
         }
 
         findViewById<CardView>(R.id.alarmBoxYellow).setOnClickListener {
             lastClickedAlarmBox = R.id.alarmBoxYellow
+            initialTime = getCurrentTimeForBox(R.id.alarmBoxYellow)
+            AMPMText = getAMPMForBox(R.id.alarmBoxYellow)
+            initialLabelText = getLabelTextForBox(R.id.alarmBoxYellow)
             navigateToSetAlarm(R.drawable.set_alarm_box_yellow, R.drawable.alarm_switch_track_on_yellow, R.drawable.set_alarm_save_button_box_yellow)
         }
 
         findViewById<CardView>(R.id.alarmBoxPink).setOnClickListener {
             lastClickedAlarmBox = R.id.alarmBoxPink
+            initialTime = getCurrentTimeForBox(R.id.alarmBoxPink)
+            AMPMText = getAMPMForBox(R.id.alarmBoxPink)
+            initialLabelText = getLabelTextForBox(R.id.alarmBoxPink)
             navigateToSetAlarm(R.drawable.set_alarm_box_pink, R.drawable.alarm_switch_track_on_pink, R.drawable.set_alarm_save_button_box_pink)
         }
     }
 
-    // AlarmList -> setAlarm
     private fun navigateToSetAlarm(boxDrawableResId: Int, switchDrawableResId: Int, buttonDrawableResId: Int) {
-        val currentTime = getCurrentTimeForBox(lastClickedAlarmBox)
         val intent = Intent(this, SetAlarm::class.java).apply {
             putExtra("EXTRA_BOX_COLOR", boxDrawableResId)
             putExtra("EXTRA_SWITCH_COLOR", switchDrawableResId)
             putExtra("EXTRA_BUTTON_COLOR", buttonDrawableResId)
-            putExtra("EXTRA_ALARM_TIME", currentTime) // AlarmList의 버튼 시간 값 전달
+            putExtra("EXTRA_ALARM_TIME", initialTime)
+            putExtra("EXTRA_AM_PM", AMPMText)
+            putExtra("EXTRA_LABEL_TEXT", initialLabelText)
         }
         requestSetAlarm.launch(intent)
     }
@@ -91,9 +70,24 @@ class AlarmList : AppCompatActivity() {
         }
     }
 
+    private fun getAMPMForBox(alarmBoxId: Int): String {
+        return when (alarmBoxId) {
+            R.id.alarmBoxBlue -> findViewById<TextView>(R.id.alarmAmPmBlue).text.toString()
+            R.id.alarmBoxYellow -> findViewById<TextView>(R.id.alarmAmPmYellow).text.toString()
+            R.id.alarmBoxPink -> findViewById<TextView>(R.id.alarmAmPmPink).text.toString()
+            else -> ""
+        }
+    }
 
+    private fun getLabelTextForBox(alarmBoxId: Int): String {
+        return when (alarmBoxId) {
+            R.id.alarmBoxBlue -> findViewById<TextView>(R.id.alarmLabelBlue).text.toString()
+            R.id.alarmBoxYellow -> findViewById<TextView>(R.id.alarmLabelYellow).text.toString()
+            R.id.alarmBoxPink -> findViewById<TextView>(R.id.alarmLabelPink).text.toString()
+            else -> ""
+        }
+    }
 
-    // SetAlarm -> AlarmList(알람 목록 페이지에서 시간 업데이트)
     private val requestSetAlarm: ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -101,43 +95,35 @@ class AlarmList : AppCompatActivity() {
             result.data?.let {
                 val selectedTime = it.getStringExtra("EXTRA_SELECTED_TIME")
                 val alarmName = it.getStringExtra("EXTRA_ALARM_NAME")
-                val isSoundVibrationOn = it.getBooleanExtra("EXTRA_SOUND_VIBRATION", false)
-                updateAlarmBox(selectedTime, alarmName, isSoundVibrationOn)
+                updateAlarmBox(selectedTime, alarmName)
             }
         }
     }
 
-    // setAlarm에서 선택한 시간을 alarmList의 UI에 반영
-    private fun updateAlarmBox(time: String?, name: String?, isSoundVibrationOn: Boolean) {
+    private fun updateAlarmBox(time: String?, name: String?) {
         val alarmBox = findViewById<CardView>(lastClickedAlarmBox)
-        val (timeTextViewId, labelTextViewId, amPmTextViewId, switchId) = getTextViewIdsForBox(lastClickedAlarmBox)
+        val textViewIds = getTextViewIdsForBox(lastClickedAlarmBox)
 
-        val timeTextView = alarmBox.findViewById<TextView>(timeTextViewId)
-        val labelTextView = alarmBox.findViewById<TextView>(labelTextViewId)
-        val amPmTextView = alarmBox.findViewById<TextView>(amPmTextViewId)
-        val soundVibrationSwitch = alarmBox.findViewById<Switch>(switchId)
+        val timeTextView = alarmBox.findViewById<TextView>(textViewIds.first)
+        val labelTextView = alarmBox.findViewById<TextView>(textViewIds.second)
+        val amPmTextView = alarmBox.findViewById<TextView>(textViewIds.third)
 
         timeTextView.typeface = ResourcesCompat.getFont(this, R.font.oxygen_bold)
         val (newTime, amPm) = splitTimeAndAmPm(time)
         timeTextView.text = newTime
         amPmTextView.text = amPm
         labelTextView.text = name
-
-        soundVibrationSwitch.isChecked = isSoundVibrationOn
     }
 
-    data class Quadruple<A, B, C, D>(val first: A, val second: B, val third: C, val fourth: D) // 기본 제공x -> 정의
-
-    private fun getTextViewIdsForBox(alarmBoxId: Int): Quadruple<Int, Int, Int, Int> {
+    private fun getTextViewIdsForBox(alarmBoxId: Int): Triple<Int, Int, Int> {
         return when (alarmBoxId) {
-            R.id.alarmBoxBlue -> Quadruple(R.id.alarmTimeBlue, R.id.alarmLabelBlue, R.id.alarmAmPmBlue, R.id.alarmSwitchBlue)
-            R.id.alarmBoxYellow -> Quadruple(R.id.alarmTimeYellow, R.id.alarmLabelYellow, R.id.alarmAmPmYellow, R.id.alarmSwitchYellow)
-            R.id.alarmBoxPink -> Quadruple(R.id.alarmTimePink, R.id.alarmLabelPink, R.id.alarmAmPmPink, R.id.alarmSwitchPink)
-            else -> Quadruple(R.id.alarmTimeBlue, R.id.alarmLabelBlue, R.id.alarmAmPmBlue, R.id.alarmSwitchBlue) // 디폴트
+            R.id.alarmBoxBlue -> Triple(R.id.alarmTimeBlue, R.id.alarmLabelBlue, R.id.alarmAmPmBlue)
+            R.id.alarmBoxYellow -> Triple(R.id.alarmTimeYellow, R.id.alarmLabelYellow, R.id.alarmAmPmYellow)
+            R.id.alarmBoxPink -> Triple(R.id.alarmTimePink, R.id.alarmLabelPink, R.id.alarmAmPmPink)
+            else -> Triple(R.id.alarmTimeBlue, R.id.alarmLabelBlue, R.id.alarmAmPmBlue)
         }
     }
 
-    // 시간 분리 메서드(시간과 AM, PM 분리)
     private fun splitTimeAndAmPm(time: String?): Pair<String, String> {
         return time?.split(" ")?.let { parts ->
             if (parts.size == 2) Pair(parts[0], parts[1]) else Pair(time, "")
