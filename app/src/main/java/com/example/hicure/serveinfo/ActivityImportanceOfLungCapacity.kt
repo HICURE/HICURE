091acec9d0d46ce44f84d932b1a4dc40f8419488
Heo Nayeong <gjsk132@naver.com>
2024-08-13 00:44:19 +0900

    FEAT : ActivityImportanceOfLungCapacity Info

--- app/src/main/java/com/example/hicure/serveinfo/ActivityImportanceOfLungCapacity.kt
+++ app/src/main/java/com/example/hicure/serveinfo/ActivityImportanceOfLungCapacity.kt
@@ -1,5 +1,7 @@
 package com.example.hicure.serveinfo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
@@ -38,11 +40,20 @@ class ActivityImportanceOfLungCapacity : AppCompatActivity() {
 
         binding.subTitle.text = infoSubTitle
 
-        binding.content1.visibility = View.GONE
         binding.content2.visibility = View.GONE
         binding.content3.visibility = View.GONE
         binding.content4.visibility = View.GONE
         binding.content5.visibility = View.GONE
         binding.image.visibility = View.GONE
+
+        binding.content1.text = "<< 운동 성능 향상 >>\n충분한 산소 공급은 운동 시 지구력과 성능을 향상시킴.\n\n" +
+                "<< 질병 예방 >>\n폐활량이 좋으면 호흡기 질환(천식, COPD 등) 예방에 도움이 됨.\n\n" +
+                "<< 심혈관 건강 >>\n산소 공급이 원활하면 심혈관 질환의 위험이 감소함.\n\n" +
+                "<< 에너지 수준 >>\n효율적인 가스 교환은 전신의 에너지 수준을 높여줌.\n\n" +
+                "<< 삶의 질 향상 >>\n좋은 폐 기능은 일상 생활에서의 피로감을 줄이고 활동성을 높임."
+
+        binding.backBtn.setOnClickListener{
+            finish()
+        }
     }
 }
