3792709872c603c651e21c408f28a96ad98d9349
Heo Nayeong <gjsk132@naver.com>
2024-08-16 05:23:41 +0900

    FIX : show Double maxValue

--- app/src/main/java/com/example/hicure/Calendar.kt
+++ app/src/main/java/com/example/hicure/Calendar.kt
@@ -117,16 +117,6 @@ class Calendar : AppCompatActivity() {
         }
     }
 
-    private fun getCurrentDate(): String {
-        val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault())
-        return dateFormat.format(Date())
-    }
-
-    private fun getCurrentTime(): String {
-        val dateFormat = SimpleDateFormat("hh : mm", Locale.getDefault())
-        return dateFormat.format(Date())
-    }
-
     private fun getUserNameFromPreferences(): String? {
         val sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE)
         return sharedPreferences.getString("user_id", null)
@@ -143,7 +133,7 @@ class Calendar : AppCompatActivity() {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val maxValue = snapshot.children.flatMap { it.children }
                     .filter { it.key != "time" }
-                    .mapNotNull { it.getValue(Int::class.java) }
+                    .mapNotNull { it.getValue(Double::class.java) }
                     .maxOrNull()
 
                 breathTextView.text = if (maxValue != null) {
